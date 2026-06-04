e0933e6e4f7dbff07037e617178f44799099a5a4
Diana <diana10august@gmail.com>
2026-06-05 00:55:46 +0300

    commit main

---
+++ main.py
@@ -0,0 +1,107 @@
+import pygame
+
+from settings import *
+from level import Level
+from player import Player
+from berry import Berry
+from score import Score
+from ghost import Ghost
+from HUD import HUD
+
+pygame.init()
+
+# ---------------- SCREEN ----------------
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pac-Man")
+
+clock = pygame.time.Clock()
+
+# ---------------- OBJECTS ----------------
+level = Level()
+player = Player(level)
+
+berries = Berry()
+score = Score()
+hud = HUD()
+
+ghosts = [
+    Ghost(5, 5, "red"),
+    Ghost(7, 9, "orange"),
+    Ghost(4, 2, "cyan"),
+    Ghost(8, 5, "pink")
+]
+
+# ---------------- GAME LOOP ----------------
+running = True
+
+while running:
+
+    clock.tick(FPS)
+
+    # -------- EVENTS --------
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+    # -------- UPDATE --------
+    player.move()
+    player.update_power_mode()
+    player.update_invincible()
+
+    points = player.eat_berry(berries)
+    if points:
+        score.add(points)
+
+    points = player.eat_big_berry(berries)
+    if points:
+        score.add(points)
+
+    # -------- WIN --------
+    if not berries.berries and not berries.big_berries:
+
+        print("YOU WIN!")
+        running = False
+
+    # -------- GHOSTS --------
+    for ghost in ghosts:
+
+        ghost.move(player)
+
+        # З'їли привида
+        if player.power_mode and player.is_dead(ghost):
+
+            ghost.reset()
+            score.add(200)
+
+        # Привид з'їв гравця
+        elif not player.invincible and player.is_dead(ghost):
+
+            player.lives -= 1
+
+            if player.lives <= 0:
+
+                print("GAME OVER")
+                running = False
+
+            else:
+
+                player.reset()
+            break
+
+    # -------- DRAW --------
+    screen.fill(BLACK)
+
+    level.draw(screen)
+    berries.draw(screen)
+
+    player.draw(screen)
+
+    for ghost in ghosts:
+        ghost.draw(screen)
+
+    hud.draw(screen, score, player)
+
+    pygame.display.flip()
+
+pygame.quit()
