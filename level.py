d2baeb49d8680c86731ea42868669541b27f3e29
Artem Myroshnik <artemmiroshniks@gmail.com>
2026-06-05 00:38:28 +0300

    upt_level

---
+++ level.py
@@ -0,0 +1,27 @@
+import pygame
+from settings import *
+from map_data import MAP
+
+
+class Level:
+
+    def __init__(self):
+        self.map = MAP
+
+    def draw(self, screen):
+
+        for row in range(len(self.map)):
+            for col in range(len(self.map[row])):
+
+                cell = self.map[row][col]
+
+                x = col * CHAR_SIZE
+                y = row * CHAR_SIZE
+
+                # стіни
+                if cell == '1':
+                    pygame.draw.rect(
+                        screen,
+                        BLUE,
+                        (x, y, CHAR_SIZE, CHAR_SIZE)
+                    )
