50d57713369f6048286db357bba0fc1b624d9177
Artem Myroshnik <artemmiroshniks@gmail.com>
2026-06-05 00:44:34 +0300

    upt_HUD

---
+++ HUD.py
@@ -0,0 +1,21 @@
+import pygame
+from settings import WHITE
+
+
+class HUD:
+
+    def __init__(self):
+
+        self.font = pygame.font.SysFont("Arial", 24)
+
+    def draw(self, screen, score, player):
+
+        # SCORE
+        score_text = self.font.render(
+            f"Score: {score.value}",
+            True,
+            WHITE
+        )
+
+        screen.blit(score_text, (10, 10))
+
